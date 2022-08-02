4ff3a4038cd2dc22181ad8b2b98b36ba4806fe7e
Alfa Nugraha <alfa.nugraha4@gmail.com>
2022-08-02 15:17:16 +0700

    Add executor

---
+++ core/defaultproviders.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+
+def loadDefaultProviders():
+    # this is here just to "trigger" the above imports so providers are loaded
+    # and can be found by the Processing.initialize() method
+    pass
